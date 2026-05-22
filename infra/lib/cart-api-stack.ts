4113b9c0555ae8d5db6eb54b266a8f0bd405a07c
Oscar Torres <oscar@oscartorres.co>
2026-05-21 23:28:13 -0500

    task-8.2: add VPC, RDS, secrets manager integration, and update Lambda setup

--- infra/lib/cart-api-stack.ts
+++ infra/lib/cart-api-stack.ts
@@ -1,21 +1,109 @@
-import * as cdk from 'aws-cdk-lib/core';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-import { aws_apigateway, aws_lambda_nodejs } from 'aws-cdk-lib';
+import {
+  aws_apigateway,
+  aws_ec2,
+  aws_lambda_nodejs,
+  aws_rds,
+  aws_secretsmanager,
+} from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { Duration } from 'aws-cdk-lib/core';
 import * as path from 'node:path';
+import { InterfaceVpcEndpointAwsService } from 'aws-cdk-lib/aws-ec2';
 
-interface CartApiStackProps extends cdk.StackProps {
+interface CartApiStackProps extends StackProps {
   prefix: string;
 }
 
-export class CartApiStack extends cdk.Stack {
+export class CartApiStack extends Stack {
   constructor(scope: Construct, id: string, props: CartApiStackProps) {
     super(scope, id, props);
 
     const { prefix } = props;
 
-    const nestJsLambda = new aws_lambda_nodejs.NodejsFunction(
+    /* Networking */
+    const vpc = new aws_ec2.Vpc(this, `${prefix}-VPC`, {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: `${prefix}-PrivateSubnet`,
+          subnetType: aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+      ],
+      natGateways: 0,
+    });
+
+    /* VPC endpoints - Creates net interfaces for AWS services */
+    vpc.addInterfaceEndpoint(`${prefix}-VPCEndpoint-SecretsManager`, {
+      service: InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+    });
+
+    /* Security Groups */
+    const lambdaSecurityGroup = new aws_ec2.SecurityGroup(
+      this,
+      `${prefix}-LambdaSecurityGroup`,
+      {
+        vpc,
+        allowAllOutbound: true,
+        description: 'Security Group for Lambda Functions',
+      },
+    );
+    const rdsSecurityGroup = new aws_ec2.SecurityGroup(
+      this,
+      `${prefix}-RdsSecurityGroup`,
+      {
+        vpc,
+        allowAllOutbound: false,
+        description: 'Security Group for RDS Database',
+      },
+    );
+
+    rdsSecurityGroup.addIngressRule(
+      lambdaSecurityGroup,
+      aws_ec2.Port.tcp(5432),
+      'Allow RDS access from Lambda',
+    );
+
+    /* Secrets */
+    const cartDbCredentialsSecret = new aws_secretsmanager.Secret(
+      this,
+      `${prefix}-RdsCredentialsSecret`,
+      {
+        secretName: 'cart-db-credentials',
+        generateSecretString: {
+          secretStringTemplate: JSON.stringify({ username: 'cloudx_admin' }),
+          generateStringKey: 'password',
+          excludePunctuation: true,
+        },
+      },
+    );
+
+    /* RDS Database */
+    const cartDbInstance = new aws_rds.DatabaseInstance(
+      this,
+      `${prefix}-RdsDatabase`,
+      {
+        engine: aws_rds.DatabaseInstanceEngine.postgres({
+          version: aws_rds.PostgresEngineVersion.VER_18_3,
+        }),
+        instanceType: aws_ec2.InstanceType.of(
+          aws_ec2.InstanceClass.BURSTABLE4_GRAVITON,
+          aws_ec2.InstanceSize.MICRO,
+        ),
+        vpc,
+        vpcSubnets: { subnetType: aws_ec2.SubnetType.PRIVATE_WITH_EGRESS },
+        securityGroups: [rdsSecurityGroup],
+        credentials: aws_rds.Credentials.fromSecret(cartDbCredentialsSecret),
+        multiAz: false,
+        allocatedStorage: 20,
+        removalPolicy: RemovalPolicy.DESTROY,
+        backupRetention: Duration.days(1),
+      },
+    );
+
+    /* Api Lambda */
+    const cartApiLambda = new aws_lambda_nodejs.NodejsFunction(
       this,
       `${prefix}-Lambda-CartApi`,
       {
@@ -24,6 +112,9 @@ export class CartApiStack extends cdk.Stack {
         runtime: Runtime.NODEJS_24_X,
         timeout: Duration.seconds(30),
         memorySize: 1024,
+        vpc,
+        vpcSubnets: { subnetType: aws_ec2.SubnetType.PRIVATE_WITH_EGRESS },
+        securityGroups: [lambdaSecurityGroup],
         bundling: {
           externalModules: [
             '@nestjs/websockets/socket-module',
@@ -35,7 +126,9 @@ export class CartApiStack extends cdk.Stack {
         },
       },
     );
+    cartDbCredentialsSecret.grantRead(cartApiLambda);
 
+    /* API Gateway */
     const apiGateway = new aws_apigateway.RestApi(
       this,
       `${prefix}-ApiGateway`,
@@ -46,7 +139,7 @@ export class CartApiStack extends cdk.Stack {
     );
 
     const cartApiIntegration = new aws_apigateway.LambdaIntegration(
-      nestJsLambda,
+      cartApiLambda,
     );
     apiGateway.root.addMethod('ANY', cartApiIntegration);
     apiGateway.root.addProxy({
